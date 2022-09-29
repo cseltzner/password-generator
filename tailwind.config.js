660161c10f0d6648ca6f921898a81addd0e08793
Chase <chase.seltz21@gmail.com>
2022-09-29 12:17:56 -0700

    Update main color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
       fontFamily: {
         sans: ["Noto Sans Mono", "monospace"],
       },
+      colors: {
+        "primary-mint": "#aff8ba",
+        "secondary-yellow": "#f4cc64",
+        "surface-dark": "#18171f",
+        "surface-light": "#24232b",
+        "text-light": "#c9c7d0",
+      },
     },
   },
   plugins: [],
