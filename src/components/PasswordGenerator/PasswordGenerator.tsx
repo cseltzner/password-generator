a77326e5829217a76d53b6e5d2c2641d356ae647
Chase <chase.seltz21@gmail.com>
2022-09-30 09:25:24 -0700

    Create PasswordGenerator file structure

---
+++ src/components/PasswordGenerator/PasswordGenerator.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PasswordGenerator = () => {
+  return <div>PasswordGenerator</div>;
+};
+
+export default PasswordGenerator;
---
+++ src/components/PasswordGenerator/PasswordOutput.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PasswordOutput = () => {
+  return <div>PasswordOutput</div>;
+};
+
+export default PasswordOutput;
