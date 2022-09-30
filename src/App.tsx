a53d2bcc06dd4be4bf78817c552420ab3cb5edfa
Chase <chase.seltz21@gmail.com>
2022-09-30 09:19:48 -0700

    Set up basic styling for page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="w-screen h-screen bg-gradient-to-t from-black to-zinc-900 text-text-light"></div>
+  );
 }
 
 export default App;
